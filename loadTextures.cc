9680aac8392f67dbb98b5c1d7163f4b6ef3a375b
Andrew J. Pounds <pounds_aj@mercer.edu>
2017-03-27 13:34:41 -0400

    Working on sphere, just added SOIL load

--- loadTextures.cc
+++ loadTextures.cc
@@ -62,6 +62,25 @@ void loadTextures(){
 
     glGenerateMipmap(GL_TEXTURE_2D); 
 
+// And now do it for the earth image for the sphere
+
+    glGenTextures(1, &textureID[2]);  
+    glBindTexture(GL_TEXTURE_2D, textureID[2]); 
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    image = SOIL_load_image("./textures/earthmap1k.jpg", &width, &height, 0, SOIL_LOAD_RGB); 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+
+    glGenerateMipmap(GL_TEXTURE_2D); 
+
+//  Clean up after ourselves
+
     SOIL_free_image_data(image);
 
     glBindTexture(GL_TEXTURE_2D, 0);   
